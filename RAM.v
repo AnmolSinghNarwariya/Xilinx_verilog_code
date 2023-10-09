cfc4ee6d81699ddff51959cad656681e66a3f9c9
Anmol Singh Narwariya <anmol8496@gmail.com>
2023-10-09 09:42:45 +0530

    Create RAM.v

---
+++ RAM.v
@@ -0,0 +1,34 @@
+module RAM(DOA, DOB, DOC, DOD,ADDRA, ADDRB, ADDRC, ADDRD, DIA, DIB, DIC, DID, WCLK, WE);
+output DOA, DOB, DOC, DOD;
+input ADDRA, ADDRB, ADDRC, ADDRD, DIA, DIB, DIC, DID, WCLK, WE;
+// RAM32M: 32-deep by 8-wide Multi Port LUT RAM (Mapped to four SliceM LUT6s)
+//         7 Series
+// Xilinx HDL Language Template, version 2021.2
+
+RAM32M #(
+   .INIT_A(64'h0000000000000000), // Initial contents of A Port
+   .INIT_B(64'h0000000000000000), // Initial contents of B Port
+   .INIT_C(64'h0000000000000000), // Initial contents of C Port
+   .INIT_D(64'h0000000000000000)  // Initial contents of D Port
+) RAM32M_inst (
+   .DOA(DOA),     // Read port A 2-bit output
+   .DOB(DOB),     // Read port B 2-bit output
+   .DOC(DOC),     // Read port C 2-bit output
+   .DOD(DOD),     // Read/write port D 2-bit output
+   .ADDRA(ADDRA), // Read port A 5-bit address input
+   .ADDRB(ADDRB), // Read port B 5-bit address input
+   .ADDRC(ADDRC), // Read port C 5-bit address input
+   .ADDRD(ADDRD), // Read/write port D 5-bit address input
+   .DIA(DIA),     // RAM 2-bit data write input addressed by ADDRD,
+                  //   read addressed by ADDRA
+   .DIB(DIB),     // RAM 2-bit data write input addressed by ADDRD,
+                  //   read addressed by ADDRB
+   .DIC(DIC),     // RAM 2-bit data write input addressed by ADDRD,
+                  //   read addressed by ADDRC
+   .DID(DID),     // RAM 2-bit data write input addressed by ADDRD,
+                  //   read addressed by ADDRD
+   .WCLK(WCLK),   // Write clock input
+   .WE(WE)        // Write enable input
+);
+
+endmodule 
